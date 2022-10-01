bbaf8c39c76f4490140544fb5b17c28a000dc7eb
nik79 <otrok2@mail.ru>
2022-10-01 18:41:59 +0700

    Рефакторинг

--- src/components/Graphics/GraphicVoltage.js
+++ src/components/Graphics/GraphicVoltage.js
@@ -1,19 +1,18 @@
 //import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import { Select } from 'antd';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const { Option } = Select;
 
-
-
 const GraphicVoltage = (props) => { 
 
-    const [voltage, setVoltage] = useState(props.calculationStatistics.voltageResultProcessed[0])
-    const handleVoltageChange = (value) => {
-      for (let i=0; i<props.calculationStatistics.voltageResultProcessed.length; i++) {
-        if (props.calculationStatistics.voltageResultProcessed[i].nodeNumber == value) {
-          setVoltage(props.calculationStatistics.voltageResultProcessed[i]);
+  const [voltage, setVoltage] = useState(0)
+    
+  const handleVoltageChange = (value) => {
+      for (let i=0; i<props.calculationStatistics.length; i++) {
+        if (props.calculationStatistics[i].nodeNumber == value) {
+          setVoltage(props.calculationStatistics[i]);
         }
       }
     };
@@ -23,14 +22,14 @@ const GraphicVoltage = (props) => {
           <h4>Напряжение</h4>
           <div className="select">
           Узел: 
-          <Select defaultValue={props.calculationStatistics.voltageResultProcessed[0].nodeNumber} style={{width: 120,}} 
+          <Select defaultValue={props.calculationStatistics[0].nodeNumber} style={{width: 120,}} 
           onChange={handleVoltageChange}>
-        {props.calculationStatistics.voltageResultProcessed?.map((voltageResultProcessed) => (
+        {props.calculationStatistics?.map((voltageResultProcessed) => (
           <Option key={voltageResultProcessed.nodeNumber}>{voltageResultProcessed.nodeNumber}</Option>
         ))}
           </Select>
           </div>
-        <BarChart width={500} height={300} data={voltage.calculationResultProcesseds} margin={{ top: 5, right: 30, left: 20, bottom: 5,}}>
+        <BarChart width={500} height={300} data={voltage.histogramData} margin={{ top: 5, right: 30, left: 20, bottom: 5,}}>
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="interval" />
           <YAxis />
